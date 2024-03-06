1a4516bd3c02bbd2b7ce65397d4eef37de2962ac
Jesper Sandell <sandell327@gmail.com>
2024-03-06 14:26:22 +0100

    Hardware accel

--- WebApi/TikTakWebAPI/Controllers/TranscodeVideoController.cs
+++ WebApi/TikTakWebAPI/Controllers/TranscodeVideoController.cs
@@ -123,10 +123,10 @@ private IActionResult GetHlsSegment(string path)
 
 private void Video240p(string filepath, string outputDir){
 
-    string command = $"ffmpeg -i \"{filepath}\" " + 
-    "-vf scale=w=426:h=240:force_original_aspect_ratio=decrease -c:a aac "+ 
+    string command = $"ffmpeg -hwaccel vulkan -i \"{filepath}\" " + 
+    "-vf scale=w=240:h=426:force_original_aspect_ratio=decrease,pad=240:426:(ow-iw)/2:(oh-ih)/2 -c:a aac "+ 
     "-ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 "+ 
-    "-keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 240k "+ 
+    "-keyint_min 48 -hls_time 2 -hls_playlist_type vod -b:v 240k "+ 
     "-maxrate 240k -bufsize 480k -b:a 64k -hls_segment_filename " + outputDir + "/240p_%d.ts " + outputDir + "/240p.m3u8";
 
 
@@ -134,10 +134,10 @@ private void Video240p(string filepath, string outputDir){
 }
 
 private void Video360p(string filepath, string outputDir){
-    string command = $"ffmpeg -i \"{filepath}\" " + 
-    "-vf scale=w=640:h=360:force_original_aspect_ratio=decrease -c:a aac "+ 
+    string command = $"ffmpeg -hwaccel vulkan -i \"{filepath}\" " + 
+    "-vf scale=w=360:h=640:force_original_aspect_ratio=decrease,pad=360:640:(ow-iw)/2:(oh-ih)/2 -c:a aac "+ 
     "-ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 "+ 
-    "-keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 800k "+ 
+    "-keyint_min 48 -hls_time 2 -hls_playlist_type vod -b:v 800k "+ 
     "-maxrate 856k -bufsize 1200k -b:a 96k -hls_segment_filename " + outputDir + "/360p_%d.ts " + outputDir + "/360p.m3u8";
 
     RunCommand(command);
@@ -145,10 +145,10 @@ private void Video360p(string filepath, string outputDir){
 
 private void Video480p(string filepath, string outputDir){
 
-    string command = $"ffmpeg -i \"{filepath}\" " + 
-    "-vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:a aac "+ 
+    string command = $"ffmpeg -hwaccel vulkan -i \"{filepath}\" " + 
+    "-vf scale=w=480:h=842:force_original_aspect_ratio=decrease,pad=480:842:(ow-iw)/2:(oh-ih)/2 -c:a aac "+ 
     "-ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 "+ 
-    "-keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 1400k "+ 
+    "-keyint_min 48 -hls_time 2 -hls_playlist_type vod -b:v 1400k "+ 
     "-maxrate 1498k -bufsize 2100k -b:a 128k -hls_segment_filename " + outputDir + "/480p_%d.ts " + outputDir + "/480p.m3u8";
 
     RunCommand(command);
@@ -156,10 +156,10 @@ private void Video480p(string filepath, string outputDir){
 
 private void Video720p(string filepath, string outputDir){
 
-    string command = $"ffmpeg -i \"{filepath}\" " + 
-    "-vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:a aac "+ 
+    string command = $"ffmpeg -hwaccel vulkan -i \"{filepath}\" " + 
+    "-vf scale=w=720:h=1280:force_original_aspect_ratio=decrease,pad=720:1280:(ow-iw)/2:(oh-ih)/2 -c:a aac "+ 
     "-ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 "+ 
-    "-keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 2800k "+ 
+    "-keyint_min 48 -hls_time 2 -hls_playlist_type vod -b:v 2800k "+ 
     "-maxrate 2996k -bufsize 4200k -b:a 128k -hls_segment_filename " + outputDir + "/720p_%d.ts " + outputDir + "/720p.m3u8";
 
     RunCommand(command);
@@ -167,10 +167,10 @@ private void Video720p(string filepath, string outputDir){
 
 private void Video1080p(string filepath, string outputDir){
 
-    string command = $"ffmpeg -i \"{filepath}\" " + 
-    "-vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease -c:a aac "+ 
+    string command = $"ffmpeg -hwaccel vulkan -i \"{filepath}\" " + 
+    "-vf scale=w=1080:h=1920:force_original_aspect_ratio=decrease,pad=1080:1920:(ow-iw)/2:(oh-ih)/2 -c:a aac "+ 
     "-ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 "+ 
-    "-keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 5000k "+ 
+    "-keyint_min 48 -hls_time 2 -hls_playlist_type vod -b:v 5000k "+ 
     "-maxrate 5350k -bufsize 7500k -b:a 192k -hls_segment_filename " + outputDir + "/1080p_%d.ts " + outputDir + "/1080p.m3u8";
 
     RunCommand(command);
@@ -184,15 +184,15 @@ private void MakeIndexFile(string filepath, string outputDir, string videoname){
                 string[] line ={  
                     "#EXTM3U",  
                     "#EXT-X-VERSION:3",  
-                    "#EXT-X-STREAM-INF:BANDWIDTH=10000,RESOLUTION=426x240",  
+                    "#EXT-X-STREAM-INF:BANDWIDTH=10000,RESOLUTION=240x426",  
                     $"../stream/{videoname}/240p.m3u8",  
-                    "#EXT-X-STREAM-INF:BANDWIDTH=420000,RESOLUTION=640x360",  
+                    "#EXT-X-STREAM-INF:BANDWIDTH=420000,RESOLUTION=360x640",  
                     $"../stream/{videoname}/360p.m3u8",  
-                    "#EXT-X-STREAM-INF:BANDWIDTH=680000,RESOLUTION=842x480",  
+                    "#EXT-X-STREAM-INF:BANDWIDTH=680000,RESOLUTION=480x842",  
                     $"../stream/{videoname}/480p.m3u8",  
-                    "#EXT-X-STREAM-INF:BANDWIDTH=1256000,RESOLUTION=1280x720",  
+                    "#EXT-X-STREAM-INF:BANDWIDTH=1256000,RESOLUTION=720x1280",  
                     $"../stream/{videoname}/720p.m3u8",  
-                    "#EXT-X-STREAM-INF:BANDWIDTH=2000000,RESOLUTION=1920x1080",  
+                    "#EXT-X-STREAM-INF:BANDWIDTH=2000000,RESOLUTION=1080x1920",  
                     $"../stream/{videoname}/1080p.m3u8"  
                 };  
                 File.WriteAllLines(path, line);
