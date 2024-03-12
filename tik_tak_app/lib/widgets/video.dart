64d05fde53dbed1fb87af247e40a138ffba39c17
Marcus Jensen <marcusproman0507@gmail.com>
2024-03-12 08:48:09 +0100

    Added comments

--- tik_tak_app/lib/widgets/video.dart
+++ tik_tak_app/lib/widgets/video.dart
@@ -1,39 +1,44 @@
-import '/imports.dart';
+import '/imports.dart'; // Import necessary packages and dependencies
 
 class VideoWidget extends StatefulWidget {
-  final List<Uri> videoUrls;
+  final List<Uri> videoUrls; // List of video URLs
 
-  const VideoWidget({super.key, required this.videoUrls});
+  const VideoWidget({Key? key, required this.videoUrls}) : super(key: key);
 
   @override
   VideoWidgetState createState() => VideoWidgetState();
 }
 
 class VideoWidgetState extends State<VideoWidget> {
-  late List<VideoPlayerController> _controllers;
-  late List<Future<void>> _initializeVideoPlayerFutures;
-  late int _currentVideoIndex;
-  late bool _isPlaying;
-  double _sliderValue = 0.0;
+  late List<VideoPlayerController> _controllers; // List of video player controllers
+  late List<Future<void>> _initializeVideoPlayerFutures; // Future list for initializing video player controllers
+  late int _currentVideoIndex; // Index of the currently displayed video
+  late bool _isPlaying; // Flag to track if video is currently playing
+  double _sliderValue = 0.0; // Value of the slider for video seek position
 
   @override
   void initState() {
     super.initState();
 
+    // Initialize video player controllers for each video URL
     _controllers = widget.videoUrls.map((url) => VideoPlayerController.networkUrl(url)).toList();
 
+    // Initialize future list for initializing video player controllers
     _initializeVideoPlayerFutures = List<Future<void>>.generate(
       widget.videoUrls.length,
       (index) => _controllers[index].initialize(),
     );
 
+    // Initialize initial values
     _currentVideoIndex = 0;
     _isPlaying = false;
 
+    // Set looping for all video player controllers
     for (var controller in _controllers) {
       controller.setLooping(true);
     }
 
+    // Start playing the first video
     _initializeVideoPlayerFutures[_currentVideoIndex].then((_) {
       setState(() {
         _isPlaying = true;
@@ -47,11 +52,13 @@ class VideoWidgetState extends State<VideoWidget> {
   void dispose() {
     super.dispose();
 
+    // Dispose all video player controllers
     for (var controller in _controllers) {
       controller.dispose();
     }
   }
 
+  // Toggle play/pause state of the video
   void _togglePlay() {
     setState(() {
       if (_isPlaying) {
@@ -69,13 +76,14 @@ class VideoWidgetState extends State<VideoWidget> {
     });
   }
 
+  // Callback function when slider value changes
   void _onSliderChanged(double value) {
     setState(() {
       _sliderValue = value;
     });
   }
 
-  // Resume playing the video when the user finishes dragging the slider
+  // Callback function when slider value change ends
   void _onSliderChangeEnd(double value) {
     setState(() {
       _sliderValue = value;
@@ -83,12 +91,15 @@ class VideoWidgetState extends State<VideoWidget> {
 
     final Duration duration = _controllers[_currentVideoIndex].value.duration;
 
+    // Seek to the specified position in the video
     _controllers[_currentVideoIndex].seekTo(Duration(milliseconds: (_sliderValue * duration.inMilliseconds).round()));
 
+    // Resume playing the video
     _controllers[_currentVideoIndex].play();
     _isPlaying = true;
   }
 
+  // Callback function for liking a video
   void _likeVideo() {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
@@ -113,9 +124,10 @@ class VideoWidgetState extends State<VideoWidget> {
                 child: Stack(
                   alignment: Alignment.center,
                   children: [
-                    VideoPlayer(_controllers[index]),
+                    VideoPlayer(_controllers[index]), // Display the video player
                     if (!_isPlaying && _currentVideoIndex == index)
-                      const Icon(Icons.play_arrow, size: 72.0, color: Colors.white),
+                      const Icon(Icons.play_arrow,
+                          size: 72.0, color: Colors.white), // Show play icon if video is paused
                   ],
                 ),
               ),
